efa837a988d3b65e47910c42d9cf5a7c55f3fe8a
huudangdev <nguyentranhuudang.blog@gmail.com>
2019-07-08 20:21:46 +0700

    Updates

---
+++ precache-manifest.ab3353984c60a93fac98a0dfae9a1b8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6883d338b6b7b7ab644813a144e2ef",
+    "url": "/movies-Search-web-app/index.html"
+  },
+  {
+    "revision": "26c39eacf9657365cf0a",
+    "url": "/movies-Search-web-app/static/css/main.720ab319.chunk.css"
+  },
+  {
+    "revision": "16a6385f42917bcffd60",
+    "url": "/movies-Search-web-app/static/js/2.71412b09.chunk.js"
+  },
+  {
+    "revision": "26c39eacf9657365cf0a",
+    "url": "/movies-Search-web-app/static/js/main.7fc1c5f0.chunk.js"
+  },
+  {
+    "revision": "1d1042c5a61a4fc6bda3",
+    "url": "/movies-Search-web-app/static/js/runtime~main.1b16c672.js"
+  }
+]);
